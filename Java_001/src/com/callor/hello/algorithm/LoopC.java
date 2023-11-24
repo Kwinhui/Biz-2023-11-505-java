d42534d7ce0b10c09182b4893d70f35ccbdf5f51
n96js <n96js@naver.com>
2023-11-24 11:48:52 +0900

    loop

---
+++ Java_001/src/com/callor/hello/algorithm/LoopC.java
@@ -0,0 +1,15 @@
+package com.callor.hello.algorithm;
+
+public class LoopC {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.print("* ");
+			if ((i + 1) % 10 == 0) {
+				System.out.println();
+			}
+
+		}
+
+	}
+
+}
--- Java_001/src/com/callor/hello/pay/PayF.java
+++ Java_001/src/com/callor/hello/pay/PayF.java
@@ -1,4 +1,4 @@
-package com.callor.hello;
+package com.callor.hello.pay;
 
 public class PayF {
 	public static void main(String[] args) {
