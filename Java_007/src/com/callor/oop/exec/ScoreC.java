4aa99681ebbcc1ca5627cdb92e006820b26d7d83
n96js <n96js@naver.com>
2023-12-04 11:49:59 +0900

    class

---
+++ Java_007/src/com/callor/oop/exec/ScoreC.java
@@ -0,0 +1,25 @@
+package com.callor.oop.exec;
+
+import com.callor.oop.service.ScoreService;
+
+public class ScoreC {
+	public static void main(String[] args) {
+
+		ScoreService[] scores = new ScoreService[10];
+
+		for (int i = 0; i < scores.length; i++) {
+			scores[i] = new ScoreService();
+		}
+		for (int i = 0; i < scores.length; i++) {
+			 
+			scores[i].scoreKor = (int)(Math.random() * 50) + 51;
+			
+			scores[i].scoreEng = (int)(Math.random() * 50) + 51;
+			
+			scores[i].scoreMath = (int)(Math.random() * 50) + 51;
+		}
+		
+		
+
+	}
+}
