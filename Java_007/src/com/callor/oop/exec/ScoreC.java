977ba9be2aae3bb80d41622820066a03c0544ba7
n96js <n96js@naver.com>
2023-12-04 16:45:44 +0900

    InputStream

--- Java_007/src/com/callor/oop/exec/ScoreC.java
+++ Java_007/src/com/callor/oop/exec/ScoreC.java
@@ -20,6 +20,15 @@ public class ScoreC {
 		}
 		
 		// 배열에 저장된 점수 화면에 출력하기
+		System.out.println("=".repeat(50));
+		System.out.println("국어\t 영어\t 수학\t 총점\t 평균");
+		System.out.println("-".repeat(50));
+		for(int i = 0; i < 5; i++) {
+			System.out.printf("%3d\t %3d\t %3d\t %3d\t %.2f\n", scores[i].scoreKor, scores[i].scoreEng,
+					scores[i].scoreMath, scores[i].getScoreTotal(), scores[i].getScoreAvg());
+		}
+		System.out.println("=".repeat(50));
+		
 		
 
 	}
