fd1c862b3b9dba908b7d4f99b41f1a3a9940302a
n96js <n96js@naver.com>
2023-12-07 11:49:29 +0900

    객체

---
+++ Java_010/src/com/callor/score/exec/ScoreB.java
@@ -0,0 +1,24 @@
+package com.callor.score.exec;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+
+public class ScoreB {
+	public static void main(String[] args) {
+		ScoreDto score = new ScoreDto();
+		NumberService numService = new NumberService();
+		
+		score.kor = numService.inputNumber("국어 점수를", 50, 100);
+		score.eng = numService.inputNumber("영어 점수를", 50, 100);
+		score.math = numService.inputNumber("수학 점수를", 50, 100);
+		
+		
+		int total = score.getTotal();
+		
+		System.out.println("총점 : " + total);
+		
+		
+		
+	}
+
+}
