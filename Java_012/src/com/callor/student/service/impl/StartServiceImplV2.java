d86a9386b6a3d419afe481ec3dc260e9b6b60866
n96js <n96js@naver.com>
2023-12-14 13:53:39 +0900

    Service

--- Java_012/src/com/callor/student/service/impl/StartServiceImplV2.java
+++ Java_012/src/com/callor/student/service/impl/StartServiceImplV2.java
@@ -1,14 +1,15 @@
 package com.callor.student.service.impl;
 
 import com.callor.student.models.MenuIndex;
+import com.callor.student.service.StudentService;
 import com.callor.student.utils.Line;
 
 public class StartServiceImplV2  extends StartServiceImplV1{
-	public StartServiceImplV2() {
+	public StartServiceImplV2(StudentService stService) {
 		// scan = new Scanner(System.in);
 		// V1의 생성자를 호출하여 V1 생성자에 있는
 		// 코드를 그대로 실행하겠다.
-		super();	// 항상 제일 위에 있어야함
+		super(stService);	// 항상 제일 위에 있어야함
 		
 		// ImplV1 에서는 st..ServiceImplV1() 을 사용하여
 		// stService 를 초기화 하였다.
--- Java_012/src/com/callor/student/service/impl/StudentServiceImplV1.java
+++ Java_012/src/com/callor/student/service/impl/StudentServiceImplV1.java
@@ -20,59 +20,94 @@ import com.callor.student.service.StudentService;
  */
 import com.callor.student.utils.Line;
 
+public class StudentServiceImplV1 implements StudentService {
 
-public class StudentServiceImplV1 implements StudentService{
-
-	// 현재 ImplV1 클래스를 다른 클래스가 상속하였을때 
+	// 현재 ImplV1 클래스를 다른 클래스가 상속하였을때
 	// protected 로 선언된 변수는 같이 상속이 된다.
 	// 즉 ImplV1 을 상속한 클래스에서는 별도로
 	// students 리스트 객체(변수)를 선언하지 않아도 된다.
 	protected List<StudentDto> students = null;
 	protected Scanner KeyBD = null;
+
 	public StudentServiceImplV1() {
 		students = new ArrayList<StudentDto>();
 		KeyBD = new Scanner(System.in);
 	}
+
 	// 학번을 매개변수로 전달받아 studetns 리스트를 검색하여
 	// 동일한 학번의 요소가 있으면 그 요소를 return
-	// 없으면 null 을 return 
+	// 없으면 null 을 return
 	protected StudentDto selectStdNum(String num) {
 		// TODO : 학생정보 찾기
-		for(StudentDto std : students) {
-			if(std.num.equals(num)) {
-				
+		for (StudentDto std : students) {
+			if (std.num.equals(num)) {
+
 				return std;
 			}
 		}
 		// 여기에 코드가 도달하면, 같은 학번이 없다.
 		return null;
 	}
-	
-	
+
+	public String newStdNum() {
+		String stdNum = "S0001";
+		if(!students.isEmpty()) {
+			
+			// students 리스트의 가장 "마지막 요소" 의 학번
+			stdNum = students.get(students.size() - 1).num;
+			
+			// num 데이터의 맨 첫번째 한개 글자를 추출하기
+			// S0100 이라면 S 만 추출하기
+			String frefix = stdNum.substring(0,1);
+			
+			// S0100 이라면 0100 만 추출하기
+			stdNum = stdNum.substring(1);
+			stdNum = String.format("%s%04d", frefix,Integer.valueOf(stdNum) + 1);
+			
+		}
+		return stdNum;
+		
+		
+		
+		
+	}
+
 	@Override
 	public boolean inputStudent() {
 		// TODO : 한 학생의 정보 입력받기
-		
+
 		// 키보드로 학생의 개별 정보들(학번, 이름... 등등)을 입력받고
 		// 임시로 저장할 배열
 		// StdIndex enum 에 선언된 요소의 개수를 세어서
 		// 그 개수를 사용하여 inputStr 배열을 생성한다.
 		String[] inputStr = new String[StdIndex.values().length];
-		for(StdIndex item : StdIndex.values()) {
-			while(true) {
-				System.out.printf("%s 입력(QUIT:종료) >> ", item);
+		for (StdIndex item : StdIndex.values()) {
+			String newStdNum = this.newStdNum();
+			
+			while (true) {
+				System.out.printf("%s(%s) 입력(QUIT:종료) >> ", item, newStdNum);
 				String str = KeyBD.nextLine();
-				if(str.equals("QUIT")) return false;
+				if (str.equals("QUIT"))
+					return false;
 				// 학번을 입력하는 경우 학번의 중복검사를 실시한다.
-				if(item.toString().equals("학번") && this.selectStdNum(str) != null) {
-					System.out.println("학번 중복");
+				if (item.toString().equals("학번")) {
+					if (str.isBlank()) {
+						System.out.printf("** 학번은 %s 를 사용함\n", newStdNum);
+					}
+
+					if (this.selectStdNum(str) != null) {
+						System.out.println("학번 중복");
+						continue;
+
+					}
+				} else if(str.isBlank()){
+					System.out.println("값을 입력해주세요.");
+					System.out.printf("%s 는 필수 항목입니다.\n", item);
 					continue;
 				}
 				inputStr[item.getIndex()] = str;
 				break;
-			}	// end while
-				
-			
+			} // end while
 
 		}
 		StudentDto stDto = new StudentDto();
@@ -93,19 +128,19 @@ public class StudentServiceImplV1 implements StudentService{
 //			if(!this.inputStudent()) break;
 //		}
 		boolean result = true;
-		while(result) {
+		while (result) {
 			Line.sLine(100);
 			result = this.inputStudent();
 			Line.sLine(100);
 		}
-		System.out.println("입력 끝");	
-		
+		System.out.println("입력 끝");
+
 	}
 
 	@Override
 	public void loadStudents() {
 		// TODO : 학생정보 읽어오기
-		
+
 	}
 
 	@Override
@@ -116,8 +151,8 @@ public class StudentServiceImplV1 implements StudentService{
 		Line.dLine(100);
 		System.out.println("학번\t이름\t학과\t학년\t전화번호\t주소");
 		Line.sLine(100);
-		
-		for(StudentDto dto : students) {
+
+		for (StudentDto dto : students) {
 			System.out.printf("%s\t", dto.num);
 			System.out.printf("%s\t", dto.name);
 			System.out.printf("%s\t", dto.dept);
@@ -126,16 +161,13 @@ public class StudentServiceImplV1 implements StudentService{
 			System.out.printf("%s\n", dto.addr);
 		}
 		Line.dLine(100);
-			
-			
 
-		
 	}
+
 	@Override
 	public void saveStudent() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
