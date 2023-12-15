caf1cc29be28f2c3c0c09a72a07048c0095424a2
n96js <n96js@naver.com>
2023-12-15 11:50:53 +0900

    student

--- Java_012/src/com/callor/student/exec/MainExA.java
+++ Java_012/src/com/callor/student/exec/MainExA.java
@@ -2,14 +2,14 @@ package com.callor.student.exec;
 
 import com.callor.student.service.StartService;
 import com.callor.student.service.StudentService;
-import com.callor.student.service.impl.StartServiceImplV2;
+import com.callor.student.service.impl.StartServiceImplV3;
 import com.callor.student.service.impl.StudentServiceImplV1;
 
 public class MainExA {
 	public static void main(String[] args) {
 		String studentDataFile = "/src/com/callor/student/data/student.txt";
 		StudentService stService = new StudentServiceImplV1();
-		StartService start = new StartServiceImplV2(stService);
+		StartService start = new StartServiceImplV3(stService);
 
 		start.startApp();
 		
